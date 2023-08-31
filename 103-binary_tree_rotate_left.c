fb4b56a63ae0388831a8d06d2959c3491f91a0c1
2Kelvin <rocketmancodes02@gmail.com>
2023-08-31 01:42:33 -0700

    Add task #103

---
+++ 103-binary_tree_rotate_left.c
@@ -0,0 +1,26 @@
+#include "binary_trees.h"
+#include <stdlib.h>
+
+/**
+  * binary_tree_rotate_left - rotate BTree left
+  * @tree: BTree root node
+  * Return: root pointer or NULL
+  */
+binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree)
+{
+	binary_tree_t *rotTree;
+
+	if (!tree)
+		return (NULL);
+	rotTree = tree->right;
+	if (!rotTree)
+		return (tree);
+	rotTree->left = tree->right;
+	if (rotTree->left)
+		rotTree->left->parent = tree;
+	rotTree->parent = tree->parent;
+	rotTree->left = tree;
+	rotTree = tree->parent;
+	return (rotTree);
+}
+
